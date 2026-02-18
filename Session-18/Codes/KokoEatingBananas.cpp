f67f5db1df6da05ed446de7078a1ae667eda738e
Santhapuri Venkata Ramana Monish <santhapurimonish7@gmail.com>
2026-02-18 21:37:31 +0530

    Add files via upload

---
+++ Session-18/Codes/KokoEatingBananas.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+    bool canFinish(vector<int>& piles, int h, int k)
+    {
+        long long hours = 0;
+
+        for(int i=0;i<piles.size();i++)
+        {
+            hours += (piles[i] + k - 1) / k;
+        }
+
+        return hours <= h;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+
+        int low = 1;
+        int high = *max_element(piles.begin(), piles.end());
+        int ans = high;
+
+        while(low <= high)
+        {
+            int mid = low + (high - low)/2;
+
+            if(canFinish(piles, h, mid))
+            {
+                ans = mid;
+                high = mid - 1;
+            }
+            else
+                low = mid + 1;
+        }
+
+        return ans;
+    }
+};
