281acc80754dfbbea794bd7c549b8677c859d3a8
Santhapuri Venkata Ramana Monish <santhapurimonish7@gmail.com>
2026-04-30 11:52:35 +0530

    Create longestplaindromicsubsequence.cpp

---
+++ LAB EST DAA/longestplaindromicsubsequence.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestPalindromeSubseq(string s) {
+    int n = s.size();
+
+    vector<vector<int>> dp(n, vector<int>(n, 0));
+    for(int i = n - 1; i >= 0; i--) {
+        dp[i][i] = 1; 
+
+        for(int j = i + 1; j < n; j++) {
+            if(s[i] == s[j]) {
+                dp[i][j] = 2 + dp[i + 1][j - 1];
+            } else {
+                dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    return dp[0][n - 1];
+}
+
+int main() {
+    string s;
+
+    cout << "Enter string: ";
+    cin >> s;
+
+    int ans = longestPalindromeSubseq(s);
+
+    cout << "Length of Longest Palindromic Subsequence:" << ans << endl;
+
+    return 0;
+}
